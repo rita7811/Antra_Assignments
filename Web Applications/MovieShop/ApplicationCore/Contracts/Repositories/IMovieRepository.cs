6d0809cf429f70fc0ed494cf4b635bb199eb5a25
rita7811 <92129512+rita7811@users.noreply.github.com>
2022-06-13 23:37:13 -0400

    assignment day21

---
+++ Web Applications/MovieShop/ApplicationCore/Contracts/Repositories/IMovieRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ApplicationCore.Contracts.Repositories
+{
+	public interface IMovieRepository
+	{
+		// all the database logic methods for Movies
+
+		// Movie GetMovie(int id)  --return type will be Entities object (b/c delimited DB)
+	}
+}
+
---
+++ Web Applications/MovieShop/MovieShopMVC/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MovieShopMVC.Models;
+
+namespace MovieShopMVC.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public IActionResult TopRatedMovies()
+    {
+        // this is going to call the movie service
+        // movie service will call movie repository (delimited business project)
+        // movie repository will call the database to get the data (delimited database project)
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
